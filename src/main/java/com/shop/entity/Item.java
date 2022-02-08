d2408a2523a663eb555593703127f565a3cd6aff
njs04210 <pearlnkim@gmail.com>
2022-02-08 15:15:59 +0900

    Implement Item entity

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,42 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@ToString
+@Table(name = "item")
+@Entity
+public class Item {
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id; // 상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm; // 상품명
+
+    @Column(nullable = false)
+    private int price; // 가격
+
+    @Column(nullable = false)
+    private int stockNumber; // 재고 수량
+
+    @Lob
+    @Column(nullable = false)
+    private String ItemDetail; // 상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellstatus; // 상품 판매 상태
+
+    private LocalDateTime regTime; // 등록시간
+
+    private LocalDateTime updateTime; // 수정시간
+
+}
