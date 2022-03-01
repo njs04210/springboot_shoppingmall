967568b1bc91446226bc97b3715bebcd029ebac0
njs04210 <pearlnkim@gmail.com>
2022-03-01 18:12:16 +0900

    Implement entity.CartItem

---
+++ src/main/java/com/shop/entity/CartItem.java
@@ -0,0 +1,30 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart_item")
+@Getter
+@Setter
+@ToString
+public class CartItem {
+
+    @Id
+    @Column(name = "cart_item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count; // 같은 상품을 장바구니에 몇 개 담을지
+}
