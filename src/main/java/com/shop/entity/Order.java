867e9429d57f3f56a928c4d26e5da4d37a0b7d4f
njs04210 <pearlnkim@gmail.com>
2022-03-01 19:32:24 +0900

    Implement entity.Order, entity.OrderItem

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,38 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order") // 연관관계 주인이 OrderItem의 필드인 order라는 의미
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate; // 주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; // 주문상태
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
---
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -0,0 +1,26 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @Column(name = "order_item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+}
